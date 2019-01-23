5e98b0950e4576dd78cec480a8878efa33a60356
Gitanshu <gitanshupruthi1234@gmail.com>
2019-01-23 10:16:43 +0530

    Calculator

---
+++ project2.py
@@ -0,0 +1,29 @@
+#making a simple calculator without function 
+
+print("welcome to our program")
+num1=int(input("enter your frst number:"))
+choices=input("choices- +,-,*,/:")
+num2=int(input("enter your second number"))
+
+if func=="+":
+	ans=num1+num2
+	print(ans)
+else:
+	pass
+if func=="-":
+	ans=num1-num2
+	print(ans)
+else:
+	pass
+if func=="*":
+	ans=num1*num2
+	print(ans)
+else:
+	pass
+if func=="/":
+	ans=num1/num2
+	print(ans)
+else:
+	pass
+print("thankew for using calculator")
+
